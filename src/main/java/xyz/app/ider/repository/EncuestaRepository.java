37808d5183af9e8c07d0c76f8c2b6a1acb769162
LuisGonzalezrubioPadilla <luis.gonzalezrubiopadilla@unicolombo.edu.co>
2024-10-20 13:10:38 -0500

    Ya crea, elimina, obtiene todas las encuestas y obtiene encuesta por id desde el backend

--- src/main/java/xyz/app/ider/repository/EncuestaRepository.java
+++ src/main/java/xyz/app/ider/repository/EncuestaRepository.java
@@ -2,6 +2,7 @@ package xyz.app.ider.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import xyz.app.ider.model.Encuesta;
 
 public interface EncuestaRepository extends JpaRepository<Encuesta, Integer> {
--- src/main/java/xyz/app/ider/service/EncuestaService.java
+++ src/main/java/xyz/app/ider/service/EncuestaService.java
@@ -1,5 +1,224 @@
 package xyz.app.ider.service;
 
-public class EncuestaService {
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
+import org.springframework.stereotype.Service;
+
+import xyz.app.ider.model.Encuesta;
+import xyz.app.ider.repository.EncuestaRepository;
+
+@Service
+public class EncuestaService implements EncuestaRepository {
+
+	@Autowired
+	private EncuestaRepository encuestaRepository;
+	
+	
+    public Encuesta crearEncuesta(Encuesta encuesta) {	// Método para crear una nueva encuesta
+        return encuestaRepository.save(encuesta);
+    }
+    
+
+    public List<Encuesta> obtenerTodasLasEncuestas() {	//Método para obtener todas las encuestas
+        return encuestaRepository.findAll();
+    }
+    
+    
+    public Optional<Encuesta> obtenerEncuestaPorId(int id) { // Método para obtener una encuesta por su id
+        return encuestaRepository.findById(id);
+    }
+    
+
+    public void eliminarEncuesta(int id) {	 // Método para eliminar una encuesta por su id
+        encuestaRepository.deleteById(id);
+    }
+	
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <S extends Encuesta> S saveAndFlush(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Encuesta> List<S> saveAllAndFlush(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAllInBatch(Iterable<Encuesta> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllByIdInBatch(Iterable<Integer> ids) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllInBatch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Encuesta getOne(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Encuesta getById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Encuesta getReferenceById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Encuesta> List<S> findAll(Example<S> example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Encuesta> List<S> findAll(Example<S> example, Sort sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Encuesta> List<S> saveAll(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Encuesta> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Encuesta> findAllById(Iterable<Integer> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Encuesta> S save(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Encuesta> findById(Integer id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public boolean existsById(Integer id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Encuesta entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllById(Iterable<? extends Integer> ids) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll(Iterable<? extends Encuesta> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Encuesta> findAll(Sort sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<Encuesta> findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Encuesta> Optional<S> findOne(Example<S> example) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public <S extends Encuesta> Page<S> findAll(Example<S> example, Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Encuesta> long count(Example<S> example) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public <S extends Encuesta> boolean exists(Example<S> example) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public <S extends Encuesta, R> R findBy(Example<S> example, Function<FetchableFluentQuery<S>, R> queryFunction) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
