b5397fb4b1903a7486155e09954f1f2c291a51b3
Juan David Martinez Gracia <juandavidmartinez576@gmail.com>
2024-10-23 20:17:10 -0500

    Capa Rest y Service de Preguntas echa, ya crea preguntas, elimina, lista todas las preguntas, tambien lits la pregunta por id, ademas trae las preguntas por id de encuesta

--- src/main/java/xyz/app/ider/rest/PreguntaRest.java
+++ src/main/java/xyz/app/ider/rest/PreguntaRest.java
@@ -1,5 +1,57 @@
 package xyz.app.ider.rest;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import xyz.app.ider.model.Pregunta;
+import xyz.app.ider.service.PreguntaService;
+
+@RestController
+@RequestMapping("/preguntas")
 public class PreguntaRest {
 
+	 @Autowired
+	 private PreguntaService preguntaService;
+	 
+	 @PostMapping
+	 public ResponseEntity<Pregunta> crearPregunta(@RequestBody Pregunta pregunta) {
+		 Pregunta nuevaPregunta = preguntaService.savePregunta(pregunta);
+	     return ResponseEntity.ok(nuevaPregunta);
+	   }
+	 
+	 @GetMapping
+	    public ResponseEntity<List<Pregunta>> listarPreguntas() {
+	        List<Pregunta> preguntas = preguntaService.getAllPreguntas();
+	        return ResponseEntity.ok(preguntas);
+	    }
+
+	 @GetMapping("/{id}")
+	    public ResponseEntity<Pregunta> obtenerPreguntaPorId(@PathVariable int id) {
+	        Optional<Pregunta> pregunta = preguntaService.getPreguntaById(id);
+	        return pregunta.map(ResponseEntity::ok)
+	                       .orElseGet(() -> ResponseEntity.notFound().build());
+	    }
+	 
+	 @DeleteMapping("/{id}")
+	    public ResponseEntity<Void> eliminarPregunta(@PathVariable int id) {
+	        preguntaService.deletePregunta(id);
+	        return ResponseEntity.noContent().build();
+	    }
+	 
+	    @GetMapping("/encuesta/{encuestaId}")
+	    public ResponseEntity<List<Pregunta>> listarPreguntasPorEncuesta(@PathVariable int encuestaId) {
+	        List<Pregunta> preguntas = preguntaService.getPreguntasByEncuestaId(encuestaId);
+	        return ResponseEntity.ok(preguntas);
+	    }
+	
 }
